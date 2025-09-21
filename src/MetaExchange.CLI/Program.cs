92a997c6da45ef456ee91a612bc5c4d93d3ddb1b
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2025-09-21 11:27:06 +0200

    init console app to parse arguments, read all orderbooks and serialize them to be a list of Exchange

--- src/MetaExchange.CLI/Program.cs
+++ src/MetaExchange.CLI/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MetaExchange.Core.Enums;
+using MetaExchange.Core.Services;
+using System.Globalization;
+
+if (args.Length < 3)
+{
+    Console.WriteLine("Usage: MetaExchange.CLI <orderbooksFolder> <BUY|SELL> <amountBtc> [outputJsonPath]");
+    Console.WriteLine("Example: dotnet run ../../orderbooks BUY 1.0 plan.json");
+    return;
+}
+
+// Parse CLI arguments
+var folder = args[0];
+if (!Enum.TryParse<Side>(args[1], true, out var side))
+{
+    Console.WriteLine("Invalid side: use BUY or SELL.");
+    return;
+}
+
+if (!decimal.TryParse(args[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var amount) 
+    || amount <= 0)
+{
+    Console.WriteLine("Invalid amountBtc.");
+    return;
+}
+
+var output = args.Length > 3 ? args[3] : null;
+
+// Load exchanges
+var exchanges = ExchangesLoader.LoadExchanges(folder);
