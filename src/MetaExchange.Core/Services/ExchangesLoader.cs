3c03ad2c74003ef6f8a8a4b830b1046514b7a985
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2025-09-21 11:19:04 +0200

    add a service to read exchanges files

---
+++ src/MetaExchange.Core/Services/ExchangesLoader.cs
@@ -0,0 +1,20 @@
+﻿using MetaExchange.Core.Models;
+using System.Text.Json;
+
+namespace MetaExchange.Core.Services;
+public static class ExchangesLoader
+{
+    public static IReadOnlyList<Exchange> LoadExchanges(string folder)
+    {
+        JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true
+        };
+
+        return [.. Directory.GetFiles(folder, "*.json")
+            .Select(f => JsonSerializer.Deserialize<Exchange>(File.ReadAllText(f), options)
+                ?? throw new InvalidOperationException($"Invalid JSON: {f}"))];
+    }
+}
